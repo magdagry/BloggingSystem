1467fe74fffe0897743d4154ff354307744fd782
Lenka <magdagry16@gmail.com>
2024-01-18 18:38:08 +0100

    searcher

--- Includes/db.php
+++ Includes/db.php
@@ -5,15 +5,15 @@ $db['db_user'] = 'root';
 $db['db_pass'] = 'root';
 $db['db_name'] = 'cms';
 
-foreach(db as $key => $value) {
-define (strtoupper($key), $value);
+foreach ($db as $key => $value) {
+    define(strtoupper($key), $value);
 }
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if($conn) {
-        die("Database connection failed: " . mysqli_connect_error());
-    } else {
-        echo ("Database is connection");
-    }
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+} else {
+    // echo "Database connected successfully";
+}
 
 ?>
