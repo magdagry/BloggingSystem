3f59ed52a64f1f553e662846efd56ae5842668e1
Lenka <magdagry16@gmail.com>
2024-01-18 20:00:16 +0100

    widget

--- Includes/sidebar.php
+++ Includes/sidebar.php
@@ -1,10 +1,31 @@
 <div class="col-md-4">
 
+<?php
+//echo $_POST['search'];
+
+if(isset($_POST['submit'])) {
+    $search = $_POST['search'];
+
+    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+    $search_query = mysqli_query($conn, $query);
+
+    if(!$search_query) {
+        die ("QUERY FAILED" . mysqli_error($conn));
+    }
+
+    $count = mysqli_num_rows($search_query);
+    if($count == 0) {
+        echo "<h1> NO RESULT</h1>"; 
+    } else {
+     echo "some result";
+    }
+}
+?>
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="search" method="post">
+                    <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
@@ -19,32 +40,27 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+
+                <?php
+                $query = "SELECT * FROM categories";
+                $select_all_categories_sidebar = mysqli_query($conn, $query);
+                ?> 
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+
+                            <?php
+                             while($row =  mysqli_fetch_assoc($select_all_categories_sidebar)) {
+                                $cat_title =  $row ['cat_title'];
+
+                                echo "<li><a href='#'>{$cat_title}</a></li>";
+                                
+                            } ?>
+                
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
+
                         </div>
                         <!-- /.col-lg-6 -->
                     </div>
@@ -52,9 +68,8 @@
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+             
+                <?php include "widget.php"; ?>
+             
 
             </div>
