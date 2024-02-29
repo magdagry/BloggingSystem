6db3027f05530a9c0733bf3606cf131f9181b6d8
Lenka <magdagry16@gmail.com>
2024-02-29 21:43:06 +0100

    comment approving

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -59,15 +59,15 @@
     $post_id = $row['post_id'];
     $post_title = $row['post_title'];
 
-    echo "<td> $post_title </td>";
+    echo "<td> <a href='/post.php?p_id=$post_id'>$post_title</a></td>";
     }
 
     // echo "<td> $post_title </td>";
 
     echo "<td>$comment_date</td>";
-    echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
-    echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
-    echo "<td><a href='posts.php?delete='>Delete</a></td>";
+    echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+    echo "<td><a href='comments.php?unapprove=$comment_id''>Unapprove</a></td>";
+    echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
     echo "./<tr>";
     }
                             
@@ -88,16 +88,45 @@
 </table>
 
 <?php
+
+if(isset($_GET['approve'])) {
+
+  echo "hallo";
+
+$the_comment_id = $_GET['approve'];
+
+    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
+    $approve_comment_query = mysqli_query($conn, $query);
+
+    header("Location: comments.php");
+    exit;
+}
+
+
+if(isset($_GET['unapprove'])) {
+
+  echo "hallo";
+
+$the_comment_id = $_GET['unapprove'];
+
+    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
+    $unapprove_comment_query = mysqli_query($conn, $query);
+
+    header("Location: comments.php");
+    exit;
+}
+
+
 if(isset($_GET['delete'])) {
 
     echo "hallo";
 
-$the_post_id = $_GET['delete'];
+$the_comment_id = $_GET['delete'];
 
-      $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+      $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id} ";
       $delete_query = mysqli_query($conn, $query);
 
-      header("Location: posts.php");
+      header("Location: comments.php");
       exit;
   }
 ?>
