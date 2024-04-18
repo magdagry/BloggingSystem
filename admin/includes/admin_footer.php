f05ce767269aad2aa7e4c59ca025063be0cce5d3
Lenka <magdagry16@gmail.com>
2024-04-18 20:55:57 +0200

    Posts

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -5,13 +5,18 @@
 
 <script src="js/jquery.js"></script>
 
+<script
+      src="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js"
+      referrerpolicy="origin"
+    ></script>
+
+<script src="js/scripts.js"></>
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
 
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
-<script src="js/scripts.js"></script>
 
 </body>
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,31 @@
-$(document).ready(function() {
-    $('#summernote').summernote({
-        height: 200
-    });
-  });
+// $(document).ready(function() {
+//     $('#summernote').summernote({
+//         height: 200
+//     });
+//   });
+
+tinymce.init({selector:'textarea'});
+
+$(document).ready(function(){
+
+    console.log("dupa");
+
+//  $('#selectAllBoxes').click(function(event){
+
+//     if(this.checked) {
+//         $('.checkBoxes').each(function() {
+
+//             this.checked = true; 
+
+//         });
+//     } else {
+//         $('.checkBoxes').each(function() {
+
+//             this.checked = false; 
+
+//         });
+
+//     }
+//  });
+
+});
